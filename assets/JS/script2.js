02d0b0898d8708be641b08c84a91b5b21f7c80b7
Jamal Tryman <https://github.com/FutureSoftwarePro>
2022-01-05 19:01:19 -0500

    Working on ALL Buttons

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -14,12 +14,28 @@ function changeButton(id) {
     
 }
 
-function changeButtonAll(id) {
-    thisBox = document.getElementById(id); 
-    thisBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
+function changeButtonAll(id1, id2, id3) {
+    let firstBox = document.getElementById(id1);
+    let secondBox = document.getElementById(id2);
+    let thirdBox = document.getElementById(id3);
+   
+    firstBox.style.background = document.getElementById(id1); 
+    firstBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
     newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
-    thisBox.style.border = "dotted 2px maroon"
-    thisBox.style.border = newBorder;
+    firstBox.style.border = "dotted 2px maroon"
+    firstBox.style.border = newBorder;
+
+    secondBox = document.getElementById(id2); 
+    secondBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
+    newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
+    secondBox.style.border = "dotted 2px maroon"
+    secondBox.style.border = newBorder;
+
+    thirdBox = document.getElementById(id3); 
+    thirdBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
+    newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
+    thirdBox.style.border = "dotted 2px maroon"
+    thirdBox.style.border = newBorder;
     
 }
 
