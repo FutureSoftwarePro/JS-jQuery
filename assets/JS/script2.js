bf0287ee6d79bd77f39a0f17b4816a466de7006e
Jamal Tryman <https://github.com/FutureSoftwarePro>
2022-01-06 15:30:58 -0500

    Buttons Fully Functional

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -9,7 +9,6 @@ function changeButton(id) {
     thisBox = document.getElementById(id);
     thisBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
     newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
-    thisBox.style.border = "dotted 2px maroon"
     thisBox.style.border = newBorder;
     
 }
@@ -22,25 +21,22 @@ function changeButtonAll(id1, id2, id3) {
     firstBox.style.background = document.getElementById(id1); 
     firstBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
     newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
-    firstBox.style.border = "dotted 2px maroon"
     firstBox.style.border = newBorder;
 
     secondBox = document.getElementById(id2); 
     secondBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
     newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
-    secondBox.style.border = "dotted 2px maroon"
     secondBox.style.border = newBorder;
 
     thirdBox = document.getElementById(id3); 
     thirdBox.style.background = allColors[Math.floor(Math.random() * allColors.length)];
     newBorder = allBorders[Math.floor(Math.random() * allColors.length)] + " 5px " + allColors[Math.floor(Math.random() * allColors.length)]
-    thirdBox.style.border = "dotted 2px maroon"
     thirdBox.style.border = newBorder;
     
 }
 
-function hideButton(id1) {
-    thisBox = document.getElementById(id1);
+function hideButton(id1,id2,id3) {
+    thisBox = document.getElementById(id1,id2,id3);
 
     if (thisBox.style.display == "none") {
         thisBox.style.display = "block";
@@ -49,6 +45,52 @@ function hideButton(id1) {
     thisBox.style.display = "none";
     }
 }
+
+function hideButtonAll(id1,id2,id3) {
+    thisBox = document.getElementById(id1);
+
+    if (thisBox.style.display == "none") {
+        thisBox.style.display = "block";
+    }
+    else {
+    thisBox.style.display = "none";
+    }
+
+    thisBox = document.getElementById(id2);
+
+    if (thisBox.style.display == "none") {
+        thisBox.style.display = "block";
+    }
+    else {
+    thisBox.style.display = "none";
+    }
+
+    thisBox = document.getElementById(id3);
+
+    if (thisBox.style.display == "none") {
+        thisBox.style.display = "block";
+    }
+    else {
+    thisBox.style.display = "none";
+    }
+}
+
+function showButtonAll(id1, id2, id3) {
+    let firstBox = document.getElementById(id1);
+    let secondBox = document.getElementById(id2);
+    let thirdBox = document.getElementById(id3);
+
+    firstBox.style.display="block";
+    secondBox.style.display="block";
+    thirdBox.style.display="block";
+    firstBox.style.display="block";
+    secondBox.style.display="block";
+    thirdBox.style.display="block";
+   
+  
+   
+}
+
 
 function resetColors(id1, id2, id3) {
     let firstBox = document.getElementById(id1);
